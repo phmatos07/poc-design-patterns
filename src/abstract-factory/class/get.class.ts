fcde96417d35ab3773aa1c2775d45ddad818512d
PHELIPE RIBEIRO DE MATOS <phelipe.matos@viavarejo.com.br>
2022-04-19 12:19:38 -0300

    feat: Criação das classes referente ao conceito 'AbstractFactory'

---
+++ src/abstract-factory/class/get.class.ts
@@ -0,0 +1,7 @@
+import { GetAbstract } from './../model/get-abstract.interface';
+
+export class Get implements GetAbstract {
+  get(url: unknown): object {
+    return {};
+  }
+}
---
+++ src/abstract-factory/class/put.class.ts
@@ -0,0 +1,7 @@
+import { PutAbstract } from './../model/put-abstract.interface';
+
+export class Put implements PutAbstract {
+  put(url: unknown): object {
+    return {};
+  }
+}
